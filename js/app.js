6c22226c9e071a89fd3448a3f48ac8a4b0e90f50
bayan <bayana.mhmd99@gmail.com>
2019-10-14 17:11:59 +0300

    sea

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+'string'
 var hours = [ ["6am: ",], ["7am: ",], ["8am: ",], ["8am: ",], ["9am: ",], ["10am: ",], ["11am: ",], ["12pm: ",],  [" 1pm: ",], [" 2pm: ",], ["3pm: ",], ["4pm: ",], [" 5pm: ",], ["6pm: ",], [" 7pm: ",], ["Total: ",] ];
 
 let Seattle = {
@@ -6,8 +6,8 @@ let Seattle = {
     max: 65,
     average: 6.3,
     arrSeattle: [],
-    EachHour: function (min, max) {
-        var range = maxr - minr;
+    EachHour: function (min, max ) {
+        var range = max - min;
         var rand = (Math.random() * range) + min;
         var final = Math.ceil(rand);
         return (final);
@@ -29,7 +29,7 @@ let Seattle = {
 var hours = [ ["6am: ",], ["7am: ",], ["8am: ",], ["8am: ",], ["9am: ",], ["10am: ",], ["11am: ",], ["12pm: ",],  [" 1pm: ",], [" 2pm: ",], ["3pm: ",], ["4pm: ",], [" 5pm: ",], ["6pm: ",], [" 7pm: ",], ["Total: ",] ];
 let Tokyo = {
     min: 3,
-    maxr: 24,
+    max: 24,
     average: 1.2,
     arrTokyo: [],
    EachHour: function (min, max) {
@@ -103,7 +103,7 @@ let Lima = {
     min: 2,
     max: 16,
     average: 4.6,
-   EachHour: function (min, maxr) {
+   EachHour: function (min, max) {
         var range = max - min;
         var rand = (Math.random() * range) + min;
         var final = Math.ceil(rand);
@@ -113,7 +113,7 @@ let Lima = {
     cookies: function () {
         var hoursSum = 0;
         for (i = 0; i < 15; i++) {
-            var cookies = this.EachHour(Seattle.minr, Seattle.max) / Seattle.average;
+            var cookies = this.EachHour(Seattle.min, Seattle.max) / Seattle.average;
             hours[i][1] = Math.ceil(cookies);
             hoursSum = hoursSum + hours[i][1];
             hours[15][1] = hoursSum;
