e8a1c19a301572576110ed506e1c3a3353fe6c33
bayan <bayana.mhmd99@gmail.com>
2019-10-15 21:49:52 +0300

    uu

--- js/app.js
+++ js/app.js
@@ -1,125 +1,55 @@
-'string'
-var hours = [ ["6am: ",], ["7am: ",], ["8am: ",], ["8am: ",], ["9am: ",], ["10am: ",], ["11am: ",], ["12pm: ",],  [" 1pm: ",], [" 2pm: ",], ["3pm: ",], ["4pm: ",], [" 5pm: ",], ["6pm: ",], [" 7pm: ",], ["Total: ",] ];
+"use strict"
+var hours = ["6am ", '7am ', '8am', '9am', '10am', '11am', '12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm '];
 
-let Seattle = {
+var seattleCookies = {
+    location: 'Seattle',
     min: 23,
     max: 65,
-    average: 6.3,
-    arrSeattle: [],
-    EachHour: function (min, max ) {
-        var range = max - min;
-        var rand = (Math.random() * range) + min;
-        var final = Math.ceil(rand);
-        return (final);
-    },
-    
-    cookies: function () {
-        var hoursSum = 0;
-        for (i = 0; i < 15; i++) {
-            var cookies = this.EachHour(Seattle.min, Seattle.max) / Seattle.average;
-            hours[i][1] = Math.ceil(cookies);
-            hoursSum = hoursSum + hours[i][1];
-            hours[15][1] = hoursSum;
-        }
-        
-        return hours;
-    }
-}
+    avgCookieSale: 6.3,
+    hourlySales: [],
+    dailySales: 0,
 
-var hours = [ ["6am: ",], ["7am: ",], ["8am: ",], ["8am: ",], ["9am: ",], ["10am: ",], ["11am: ",], ["12pm: ",],  [" 1pm: ",], [" 2pm: ",], ["3pm: ",], ["4pm: ",], [" 5pm: ",], ["6pm: ",], [" 7pm: ",], ["Total: ",] ];
-let Tokyo = {
-    min: 3,
-    max: 24,
-    average: 1.2,
-    arrTokyo: [],
-   EachHour: function (min, max) {
-        var range = max - min;
-        var rand = (Math.random() * range) + min;
-        var final = Math.ceil(rand);
-        return (final);
+    randomCust: function () {
+        var range = seattleCookies.max - seattleCookies.min;
+        var randomCount = Math.random() * range + seattleCookies.min;
+        return Math.ceil(randomCount);
     },
 
-    cookies: function () {
-        var hoursSum = 0;
-        for (i = 0; i < 15; i++) {
-            var cookies = this.EachHour(Seattle.min, Seattle.max) / Seattle.average;
-            hours[i][1] = Math.ceil(cookies);
-            hoursSum = hoursSum + hours[i][1];
-            hours[15][1] = hoursSum;
+    hourlyDailySales: function () {
+        for (var i = 0; i < hours.length; i++) {
+            var numOfCookies = Math.ceil(seattleCookies.randomCust() * seattleCookies.avgCookieSale);
+            seattleCookies.hourlySales.push(numOfCookies);
+            seattleCookies.dailySales += numOfCookies;
         }
-
-        return hours;
-    }
-}
-
-let Dubai = {
-    min: 11,
-    max: 38,
-    average: 3.7,
-    EachHour: function (min, max) {
-        var range = max - min;
-        var rand = (Math.random() * range) + min;
-        var final = Math.ceil(rand);
-        return (final);
     },
 
-    cookies: function () {
-        var hoursSum = 0;
-        for (i = 0; i < 15; i++) {
-            var cookies = this.EachHour(Seattle.min, Seattle.max) / Seattle.average
-            hoursSum = hoursSum + hours[i][1];
-            hours[15][1] = hoursSum;
+    render: function () {
+        var container = document.getElementById('content-area');
+        var h2 = document.createElement('h2');
+         container .appendChild(h2);
+        h2.textContent = 'Seattle';
+        container.appendChild(h2);
+        var list = document.createElement('ul');
+        container.appendChild(list);
+        for (var i = 0; i < hours.length; i++) {
+            var listItem = document.createElement('li');
+            listItem.textContent = hours[i] + ': ' + seattleCookies.hourlySales[i] + ' cookies';
+            list.appendChild(listItem);
         }
+        var listItem = document.createElement('li');
+        listItem.textContent = 'Total: ' + seattleCookies.dailySales + ' cookies';
+        list.appendChild(listItem);
 
-        return hours;
-    }
-}
 
-let Paris = {
-    min: 20,
-    max: 38,
-    average: 2.3,
-   EachHour: function (min, max) {
-        var range = max - min;
-        var rand = (Math.random() * range) + min;
-        var final = Math.ceil(rand);
-        return (final);
     },
-
-    cookies: function () {
-        var hoursSum = 0;
-        for (i = 0; i < 15; i++) {
-            var cookies = this.EachHour(Seattle.min, Seattle.max) / Seattle.average;
-            hours[i][1] = Math.ceil(cookies);
-            hoursSum = hoursSum + hours[i][1];
-            hours[15][1] = hoursSum;
-        }
-
-        return hours;
-    }
 }
 
-let Lima = {
-    min: 2,
-    max: 16,
-    average: 4.6,
-   EachHour: function (min, max) {
-        var range = max - min;
-        var rand = (Math.random() * range) + min;
-        var final = Math.ceil(rand);
-        return (final);
-    },
 
-    cookies: function () {
-        var hoursSum = 0;
-        for (i = 0; i < 15; i++) {
-            var cookies = this.EachHour(Seattle.min, Seattle.max) / Seattle.average;
-            hours[i][1] = Math.ceil(cookies);
-            hoursSum = hoursSum + hours[i][1];
-            hours[15][1] = hoursSum;
-        }
+seattleCookies.hourlyDailySales();
+seattleCookies.render();
 
-        return hours;
-    }
-}
+
+
+
+
 
